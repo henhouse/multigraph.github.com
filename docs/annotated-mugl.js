c3236e9982fa5c18ce8aa29a259fa633efbf1ee0
Mark Phillips <embeepea@git>
2012-12-17 14:35:44 -0800

    more annotated mugl edits

--- docs/annotated-mugl.js
+++ docs/annotated-mugl.js
@@ -13,4 +13,18 @@ $('document').ready(function() {
           collapsed_span.show();
       });
   });
+
+  $('.boolean').tooltip({
+	track: true,
+	delay: 0,
+	showURL: false,
+	extraClass: "pretty",
+	fixPNG: true,
+	opacity: 0.95,
+	left: -120,
+	  bodyHandler : function() {
+		return $('<div>Here is a<br/>test with<br/>three lines</div>');
+	}
+  });
+
 });
